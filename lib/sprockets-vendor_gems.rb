a198119060f0b4f1131df88b107ce98bc13eac5c
John Bintz <john@coswellproductions.com>
2012-07-02 20:45:04 -0400

    a small update

--- lib/sprockets-vendor_gems.rb
+++ lib/sprockets-vendor_gems.rb
@@ -2,22 +2,22 @@ require 'sprockets'
 require 'rubygems'
 
 module ::Sprockets
-  class << self
-    def find_gem_vendor_paths(options = {})
-      for_types = [ options[:for] || [ 'javascripts', 'stylesheets' ] ].flatten
+  def self.find_gem_vendor_paths(options = {})
+    options = { :paths => %w{vendor lib app} }.merge(options)
 
-      paths = []
+    for_types = [ options[:for] || [ 'javascripts', 'stylesheets' ] ].flatten
 
-      Gem::Specification.each do |gemspec|
-        %w{vendor lib app}.product(for_types).each do |base_dir, type|
-          path = File.join(gemspec.gem_dir, base_dir, "assets", type.to_s)
+    paths = []
 
-          paths << path if File.directory?(path)
-        end
+    Gem::Specification.each do |gemspec|
+      options[:paths].product(for_types).each do |base_dir, type|
+        path = File.join(gemspec.gem_dir, base_dir, "assets", type.to_s)
+
+        paths << path if File.directory?(path)
       end
-
-      paths
     end
+
+    paths
   end
 
   class EnvironmentWithVendoredGems < Environment
